df33f387983856c2433c9ec68ca76fe26c480011
Tanner Grimes <tannergrimes12@gmail.com>
2020-03-23 13:55:53 -0500

    Fixed layout added to TitleBlock

--- app/blocks/title_block.py
+++ app/blocks/title_block.py
@@ -1,10 +1,15 @@
-from wagtail.core.blocks import StructBlock
+from wagtail.core.blocks import StructBlock, CharBlock
 
+from app.widgets import CustomRadioSelect
+from .custom_choice_block import CustomChoiceBlock
+from app.choices import SWITCHABLE_CHOICES, WIDTH_CHOICES
 from .multi_text_block import MultiTextBlock
 from .background_block import BackgroundBlock
 
 
 class TitleBlock(StructBlock):
+    layout = CustomChoiceBlock(choices=WIDTH_CHOICES, default=WIDTH_CHOICES[1][0], widget=CustomRadioSelect, label='Layout')
+    switchable = CustomChoiceBlock(choices=SWITCHABLE_CHOICES, default=SWITCHABLE_CHOICES[0][0], widget=CustomRadioSelect)
     text = MultiTextBlock()
     background = BackgroundBlock()
 
