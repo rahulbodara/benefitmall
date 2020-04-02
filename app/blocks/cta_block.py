3a6dce34e240c4e066c68f11c48018ef3d619ea3
Ryan Mattes <ryanm@horsefish.net>
2020-04-02 15:54:03 -0500

    Add header size adjustments to cta block

--- app/blocks/cta_block.py
+++ app/blocks/cta_block.py
@@ -10,7 +10,7 @@ from .background_block import BackgroundBlock
 class CtaHorizontalBlock(StructBlock):
     layout = CustomChoiceBlock(choices=CTA_LAYOUT_CHOICES, default=CTA_LAYOUT_CHOICES[1][0], required=True, widget=CustomRadioSelect, label='Layout')
     header = TextBlock()
-    subhead_size = CustomChoiceBlock(choices=SUBHEAD_SIZE_CHOICES, default=SUBHEAD_SIZE_CHOICES[1][0], required=False, widget=CustomRadioSelect, label='Size')
+    subhead_size = CustomChoiceBlock(choices=SUBHEAD_SIZE_CHOICES, default=SUBHEAD_SIZE_CHOICES[1][0], required=False, widget=CustomRadioSelect, label='Header Size')
     body = TextBlock(required=False, label='Body')
     body_size = CustomChoiceBlock(choices=PARAGRAPH_SIZE_CHOICES, default=PARAGRAPH_SIZE_CHOICES[1][0], required=False, widget=CustomRadioSelect, label='Body Size')
     outline = CustomChoiceBlock(choices=OUTLINE_CHOICES, default=OUTLINE_CHOICES[1][0], required=False, widget=CustomRadioSelect, label='Outline')
