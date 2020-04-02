9da38c552ebc90cf22db185ae78ae4230f5cb2a6
Ryan Mattes <ryanm@horsefish.net>
2020-04-02 11:42:32 -0500

    bio items add links and header size options

--- app/blocks/bio_list_item_block.py
+++ app/blocks/bio_list_item_block.py
@@ -1,10 +1,11 @@
-from wagtail.core.blocks import StructBlock, CharBlock, TextBlock
+from wagtail.core.blocks import StructBlock, CharBlock, TextBlock, PageChooserBlock
 from wagtail.images.blocks import ImageChooserBlock
 
 
 class BioListItemBlock(StructBlock):
     name = CharBlock()
     title = CharBlock(required=False)
+    link = PageChooserBlock(required=False)
     image = ImageChooserBlock(required=False)
 
     class Meta:
