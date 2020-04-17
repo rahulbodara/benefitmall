7e87c692312ff964d6f4e0dd48df41d02b7cb087
Ryan Mattes <>
2020-04-17 10:32:24 -0500

    Fix vidyard media block

--- app/blocks/media_title_block.py
+++ app/blocks/media_title_block.py
@@ -5,15 +5,15 @@ from app.choices import SWITCHABLE_CHOICES, VERTICAL_ALIGNMENT_CHOICES, MEDIA_TI
 from app.widgets import CustomRadioSelect
 from .custom_choice_block import CustomChoiceBlock
 from .background_block import BackgroundBlock
-from .media_block import MediaBlock
-from .vidyard_media_block import VidyardMediaBlock
+# from .media_block import MediaBlock
+from .vidyard_media_block import MediaBlock
 from .multi_text_block import MultiTextBlock
 
 
 class MediaTitleBlock(StructBlock):
     layout = CustomChoiceBlock(choices=MEDIA_TITLE_LAYOUT_CHOICES, default=MEDIA_TITLE_LAYOUT_CHOICES[0][0], required=False, widget=CustomRadioSelect)
     switchable = CustomChoiceBlock(choices=SWITCHABLE_CHOICES, default=SWITCHABLE_CHOICES[0][0], required=False, widget=CustomRadioSelect)
-    media = VidyardMediaBlock()
+    media = MediaBlock()
     small_image = ImageChooserBlock(required=False, label='Small Image')
     vertical_alignment = CustomChoiceBlock(choices=VERTICAL_ALIGNMENT_CHOICES, default=VERTICAL_ALIGNMENT_CHOICES[0][0], required=False, widget=CustomRadioSelect, label='Vertical Alignment')
     text = MultiTextBlock()
