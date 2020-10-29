e726b5b5dd1e7c24178003944cc2463c0946cd03
Michael Guerrero <mguerrero@insite.net>
2020-10-29 17:04:03 -0500

    adding timezone dropdown and display to events

--- app/choices/model_choices.py
+++ app/choices/model_choices.py
@@ -3,3 +3,10 @@ EVENT_LOCATION_TYPE_CHOICES = (
     ('online', 'Online'),
     ('in_person', 'In Person'),
 )
+
+TIME_ZONE_CHOICES = (
+    ('US/Eastern', 'EST'),
+    ('US/Central', 'CST'),
+    ('US/Mountain', 'MST'),
+    ('US/Pacific', 'PST'),
+)
